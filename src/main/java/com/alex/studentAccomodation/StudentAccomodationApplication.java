70505c69f89d8322194b2b8f73ed26f3f983953d
Alexander-Muendesi <u20426918@tuks.co.za>
2024-11-06 20:41:44 +0200

    Initial Project Setup

---
+++ src/main/java/com/alex/studentAccomodation/StudentAccomodationApplication.java
@@ -0,0 +1,13 @@
+package com.alex.studentAccomodation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAccomodationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAccomodationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/studentAccomodation/StudentAccomodationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.studentAccomodation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAccomodationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
