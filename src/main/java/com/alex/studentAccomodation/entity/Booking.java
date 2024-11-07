62e9b97009bc0fcdb7401e70a95422dfdbbeda16
Alexander-Muendesi <u20426918@tuks.co.za>
2024-11-07 18:33:44 +0200

    Finished adding remaining entity classes

--- src/main/java/com/alex/studentAccomodation/entity/Booking.java
+++ src/main/java/com/alex/studentAccomodation/entity/Booking.java
@@ -1,4 +1,32 @@
 package com.alex.studentAccomodation.entity;
 
+import com.alex.studentAccomodation.constants.BookingStatus;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id",nullable = false)//foreign key to User entity for STUDENT
+    private User student;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Accomodation accomodation; // Foreign key to Accomodation entity
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
 }
--- src/main/java/com/alex/studentAccomodation/entity/Review.java
+++ src/main/java/com/alex/studentAccomodation/entity/Review.java
@@ -1,4 +1,26 @@
 package com.alex.studentAccomodation.entity;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id; //foreign key to User entity for STUDENT
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Accomodation accomodation; // foreign key to Accomodation entity
+
+    private int rating;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
 }
